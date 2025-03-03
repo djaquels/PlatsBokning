de909259d0572224ee9fa1443acd4872aa223a1b
Hector Jacales <hjacales@gmail.com>
2025-03-03 16:07:35 +0100

    chore: clean depoyment options

--- run.sh
+++
@@ -1,4 +0,0 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $(terraform output -raw ecr_repo_url)
-docker build -t rails-app .
-docker tag rails-app:latest $(terraform output -raw ecr_repo_url):latest
-docker push $(terraform output -raw ecr_repo_url):latest
